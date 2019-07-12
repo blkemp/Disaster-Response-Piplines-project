76e3c384855e97dce5e640a4285166b98805b19d
blkemp <brian.l.kemp@gmail.com>
2019-07-12 12:35:29 +1000

    Significant updates after testing load and transform features to ensure functionality
    f1 score now around 0.59 - possible to get to 0.64 using SVM but much slower

--- data/process_data.py
+++ data/process_data.py
@@ -14,12 +14,18 @@ def load_data(messages_filepath, categories_filepath):
     # read in messages data 
     df = pd.read_csv(messages_filepath)
     #OneHotEncode genre column
-    messages = messages.merge(
-        pd.get_dummies(messages, columns=['genre'], drop_first=False).drop(['message','original'], axis=1),
+    df = df.merge(
+        pd.get_dummies(df, columns=['genre'], drop_first=False).drop(['message','original'], axis=1),
         on='id')
 
     # load categories data
     categories = pd.read_csv(categories_filepath)
+
+    # create a dataframe of the 36 individual category columns
+    categories = pd.concat([categories, 
+                            categories['categories'].str.split(';',expand=True)],
+                            axis=1).drop('categories', axis=1)
+
     # use first row to extract a list of new column names for categories.
     row = categories.iloc[0,1:]
     category_colnames = [item[0] for item in np.array(row.str.split('-'))]
@@ -31,7 +37,7 @@ def load_data(messages_filepath, categories_filepath):
             categories[column] = categories.loc[:,column].str[-1].astype('int')
     
     # merge category data with raw messages
-    df.merge(categories, on='id')
+    df = df.merge(categories, on='id')
     return df
 
 def clean_data(df):
@@ -41,8 +47,8 @@ def clean_data(df):
     response_columns = set(df.columns) - set({'id','message','original','genre'})
     # Ensure response columns are binary
     for column in response_columns:
-        a = np.array(df[response_columns].values.tolist())
-        df[response_columns] = ((a > 0)*1).tolist()
+        a = np.array(df[column].values.tolist())
+        df[column] = ((a > 0)*1).tolist()
 
     # drop columns with no variation     
     for column in response_columns:
@@ -54,49 +60,10 @@ def clean_data(df):
     #drop_columns -= response_columns
     #df.drop(drop_columns, axis=1, inplace=True)
 
-    # Feature engineering
-    df['word_count'] = df['message'].apply(lambda x: len(x.split()))
-    df['title_word_count'] = df['message'].apply(lambda x: len([wrd for wrd in x.split() if wrd.istitle()]))
-    df['noun_count'] = df['message'].apply(lambda x: check_pos_tag(x, 'noun'))
-    df['verb_count'] = df['message'].apply(lambda x: check_pos_tag(x, 'verb'))
-    df['adj_count'] = df['message'].apply(lambda x: check_pos_tag(x, 'adj'))
-    df['adv_count'] = df['message'].apply(lambda x: check_pos_tag(x, 'adv'))
-    df['pron_count'] = df['message'].apply(lambda x: check_pos_tag(x, 'pron'))
-    # Scale engineered features
-    sc_features = ['word_count','title_word_count','noun_count','verb_count','adj_count',
-                    'adv_count','pron_count']
-    for feature in sc_features:
-        df[feature] = minmax_scale(df[feature])
-
     return df
 
-def check_pos_tag(x, flag):
-    '''Function for returning number of pos_tag items in given text
-    Inputs: 
-    x = text
-    flag = word type indicator for pos_tag reference, e.g. 'noun','verb', etc. 
-    '''
-
-    pos_family = {
-            'noun' : ['NN','NNS','NNP','NNPS'],
-            'pron' : ['PRP','PRP$','WP','WP$'],
-            'verb' : ['VB','VBD','VBG','VBN','VBP','VBZ'],
-            'adj' :  ['JJ','JJR','JJS'],
-            'adv' : ['RB','RBR','RBS','WRB']
-            }
-
-    count = 0
-    try:
-        text_types = nltk.pos_tag(tokenize(df.message[1]))
-        for word, word_pos_fam in text_types:
-            if word_pos_fam in pos_family[flag]:
-                count += 1
-    except:
-        pass
-    return count
-
 def save_data(df, database_filename):
-    engine = create_engine(database_filename)
+    engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('InsertTableName', engine, index=False) 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,25 +1,157 @@
 import sys
+# import libraries
+import numpy as np
+import pandas as pd
+import nltk
+import numpy as np
+import pandas as pd
+import sqlalchemy
+import pickle
 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.externals import joblib 
+from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
+
+class BasicTextAnalytics(BaseEstimator, TransformerMixin):
+    '''
+    Class for returning some basic numerical data for text analysis to include in 
+    modelling. Such as: 
+    - Number of sentences
+    - Number of words
+    - Number of nouns
+    - Number of verbs
+    - Number of adjectives
+    A lot of the above were taken from ideas found here: 
+    https://www.analyticsvidhya.com/blog/2018/04/a-comprehensive-guide-to-understand-and-implement-text-classification-in-python/
+    '''
+    pos_family = {
+    'noun' : ['NN','NNS','NNP','NNPS'],
+    'pron' : ['PRP','PRP$','WP','WP$'],
+    'verb' : ['VB','VBD','VBG','VBN','VBP','VBZ'],
+    'adj' :  ['JJ','JJR','JJS'],
+    'adv' : ['RB','RBR','RBS','WRB']
+    }
+
+    # function to check and get the part of speech tag count of a words in a given sentence
+    def check_pos_tag(self, text, flag):
+        '''
+        Returns the count of a given NL pos_tag, based on user selection. E.g. number of nouns.
+        INPUTS
+        text - the given text to analyse
+        flag - pos family to analyse, one of 'noun', 'pron' , 'verb', 'adj' or 'adv'
+        '''
+        count = 0
+        try:
+            wiki = textblob.TextBlob(text)
+            for tup in wiki.tags:
+                ppo = list(tup)[1]
+                if ppo in pos_family[flag]:
+                    count += 1
+        except:
+            pass
+        return count
+    
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        trainDF = pd.DataFrame()
+        trainDF['text'] = X
+        trainDF['word_count'] = trainDF['text'].apply(lambda x: len(x.split()))
+        trainDF['title_word_count'] = trainDF['text'].apply(lambda x: len([wrd for wrd in x.split() if wrd.istitle()]))
+        trainDF['noun_count'] = trainDF['text'].apply(lambda x: self.check_pos_tag(x, 'noun'))
+        trainDF['verb_count'] = trainDF['text'].apply(lambda x: self.check_pos_tag(x, 'verb'))
+        trainDF['adj_count'] = trainDF['text'].apply(lambda x: self.check_pos_tag(x, 'adj'))
+        trainDF['adv_count'] = trainDF['text'].apply(lambda x: self.check_pos_tag(x, 'adv'))
+        trainDF['pron_count'] = trainDF['text'].apply(lambda x: self.check_pos_tag(x, 'pron'))
+        
+        return trainDF.drop('text',axis=1)
 
 def load_data(database_filepath):
+    # load from database
+    engine = sqlalchemy.create_engine(database_filepath)
+    df = pd.read_sql_table('InsertTableName', engine)
     
+    # split input and response variables
+    X = df['message']
+    Y = df.drop(['id','message','original','genre'], axis=1)
+    category_names = Y.columns
     return X, Y, category_names
 
-
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    # lemmatize and remove stop words
+    stop_words = stopwords.words("english")
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    clean_tokens = [tok.lower().strip() for tok in tokens]
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    # Build and scale engineered features separate to Natural Language transformations
+    # We create the preprocessing pipelines for both numeric and text data.
 
+    pipeline_model = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize, 
+                                        ngram_range=(1, 2),
+                                        max_features=5000,
+                                        max_df=0.5)),
+                ('tfidf', TfidfTransformer())
+            ])),
 
-def evaluate_model(model, X_test, Y_test, category_names):
+            ('numerical_pipeline', Pipeline([
+                ('analytics', BasicTextAnalytics()),
+                ('norm', StandardScaler())
+                ]))
+        ])),
+
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=100, random_state=42)))
+    ])
+    # specify parameters for grid search
+    parameters = {
+        'clf__estimator__min_samples_split' : [4,8,16,32],
+        'clf__estimator__max_depth': [None,50,100]
+    }
+
+    # create grid search object
+    # using f1 score rather than auc because of the significant imbalance in class distributions
+    # per https://machinelearningmastery.com/roc-curves-and-precision-recall-curves-for-classification-in-python/
+    cv = GridSearchCV(pipeline_model, parameters, scoring='f1_weighted', verbose=3)
+
+    return cv
+
+def evaluate_model(model, X_test, Y_test):
+    # Note: this function used to have a variable "category_names" per the default
+    # I'm not sure it was necessary so I have removed it.
+
+    y_pred = model.predict(X_test)
+    
+    print('f1 score: {}'.format(f1_score(Y_test, y_pred, average='weighted')))
     pass
 
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath) 
 
 
 def main():
@@ -36,7 +168,8 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, Y_test)
+        # Note: used to also include parameter category_names
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
@@ -47,7 +180,7 @@ def main():
         print('Please provide the filepath of the disaster messages database '\
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py /data/DisasterResponse.db classifier.pkl')
 
 
 if __name__ == '__main__':
